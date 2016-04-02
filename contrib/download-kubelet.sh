a5e37c640413f4fcf67d919f14524398da696144
Brian Waldon <brian@waldon.cc>
2016-04-02 08:04:04 -0700

    bump to kubernetes v1.2.1

--- contrib/download-kubelet.sh
+++ contrib/download-kubelet.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -ex
 
+version="v1.2.1"
+
 origin=`pwd`
 
 cd /tmp
 
-wget https://github.com/kubernetes/kubernetes/releases/download/v1.2.0/kubernetes.tar.gz
+wget https://github.com/kubernetes/kubernetes/releases/download/${version}/kubernetes.tar.gz
 tar -xf kubernetes.tar.gz kubernetes/server/kubernetes-server-linux-amd64.tar.gz
 tar -xf kubernetes/server/kubernetes-server-linux-amd64.tar.gz kubernetes/server/bin/kubelet
 mv kubernetes/server/bin/kubelet $origin/roles/kubelet/files/kubelet
