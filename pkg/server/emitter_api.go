97af5b0aa357200c9bde15ce22a88b1141c65c61
Brian Waldon <brian@waldon.cc>
2016-04-16 19:54:19 -0700

    Report resource capacity/allocatable/scheduled

--- pkg/server/emitter_api.go
+++ pkg/server/emitter_api.go
@@ -4,11 +4,26 @@ import (
 	"log"
 
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/resource"
 	"k8s.io/kubernetes/pkg/client/restclient"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
 	clientcmd "k8s.io/kubernetes/pkg/client/unversioned/clientcmd"
 	clientcmdapi "k8s.io/kubernetes/pkg/client/unversioned/clientcmd/api"
+	"k8s.io/kubernetes/pkg/fields"
 )
+
+var trackedResources = []api.ResourceName{
+	api.ResourceCPU,
+	api.ResourceMemory,
+}
+
+func updateResourceList(dest, src api.ResourceList) {
+	for _, res := range trackedResources {
+		qty, _ := dest[res]
+		qty.Add(src[res])
+		dest[res] = qty
+	}
+}
 
 func getKubernetesClientConfig(kubeconfig string) (*restclient.Config, error) {
 	if kubeconfig == "" {
@@ -56,10 +71,73 @@ func (e *apiEmitter) Emit() (MetricsBundle, error) {
 }
 
 func (e *apiEmitter) emit(ch chan<- Metric) {
-	resp, err := e.client.Namespaces().List(api.ListOptions{})
+	nodeList, err := e.client.Nodes().List(api.ListOptions{})
 	if err != nil {
-		log.Printf("failed gathering namespace data: %v", err)
+		log.Printf("failed to get nodes: %v", err)
+		return
+	}
+	ch <- Metric{Name: "kubernetes.node.all", Value: len(nodeList.Items)}
+
+	capacity := make(map[api.ResourceName]resource.Quantity)
+	allocatable := make(map[api.ResourceName]resource.Quantity)
+	scheduled := make(map[api.ResourceName]resource.Quantity)
+
+	for _, n := range nodeList.Items {
+		updateResourceList(capacity, n.Status.Capacity)
+		updateResourceList(allocatable, n.Status.Allocatable)
+
+		podList, err := e.client.Pods(api.NamespaceAll).List(api.ListOptions{
+			FieldSelector: fields.OneTermEqualSelector("spec.nodeName", n.Name),
+		})
+		if err != nil {
+			log.Printf("failed to get pods for node %v: %v", n.Name, err)
+			return
+		}
+		for _, p := range podList.Items {
+			for _, c := range p.Spec.Containers {
+				updateResourceList(scheduled, c.Resources.Requests)
+			}
+		}
 	}
 
-	ch <- Metric{Name: "kubernetes.namespace.total", Value: len(resp.Items)}
+	capMem := capacity[api.ResourceMemory]
+	capCPU := capacity[api.ResourceCPU]
+	ch <- Metric{
+		Name:  "kubernetes.resource.cpu.capacity",
+		Value: int(capCPU.MilliValue()),
+	}
+	ch <- Metric{
+		Name:  "kubernetes.resource.memory.capacity",
+		Value: int(capMem.Value()),
+	}
+
+	allocMem := allocatable[api.ResourceMemory]
+	allocCPU := allocatable[api.ResourceCPU]
+	ch <- Metric{
+		Name:  "kubernetes.resource.cpu.allocatable",
+		Value: int(allocCPU.MilliValue()),
+	}
+	ch <- Metric{
+		Name:  "kubernetes.resource.memory.allocatable",
+		Value: int(allocMem.Value()),
+	}
+
+	schedMem := scheduled[api.ResourceMemory]
+	schedCPU := scheduled[api.ResourceCPU]
+	ch <- Metric{
+		Name:  "kubernetes.resource.cpu.scheduled",
+		Value: int(schedCPU.MilliValue()),
+	}
+	ch <- Metric{
+		Name:  "kubernetes.resource.memory.scheduled",
+		Value: int(schedMem.Value()),
+	}
+
+	namespaceList, err := e.client.Namespaces().List(api.ListOptions{})
+	if err != nil {
+		log.Printf("failed to get namespaces: %v", err)
+		return
+	}
+
+	ch <- Metric{Name: "kubernetes.namespace.all", Value: len(namespaceList.Items)}
 }
