4bb9f4797977620c297147d49058758662607052
Brian Waldon <brian@waldon.cc>
2016-05-16 12:24:22 -0700

    Testing of flagutil.KVSliceFlag

---
+++ pkg/flagutil/kvslice_test.go
@@ -0,0 +1,67 @@
+package flagutil
+
+import (
+	"testing"
+
+	"github.com/kylelemons/godebug/pretty"
+)
+
+func TestKVSliceSet(t *testing.T) {
+	tests := []struct {
+		arg  string
+		want [][2]string
+	}{
+		{
+			arg: "foo=bar",
+			want: [][2]string{
+				[2]string{"foo", "bar"},
+			},
+		},
+		{
+			arg: "foo=bar,ping=pong",
+			want: [][2]string{
+				[2]string{"foo", "bar"},
+				[2]string{"ping", "pong"},
+			},
+		},
+		{
+			arg: " foo = bar , ping = pong ",
+			want: [][2]string{
+				[2]string{" foo ", " bar "},
+				[2]string{" ping ", " pong "},
+			},
+		},
+		{
+			arg: "foo=",
+			want: [][2]string{
+				[2]string{"foo", ""},
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		var f KVSliceFlag
+		if err := f.Set(tt.arg); err != nil {
+			t.Errorf("case %d: unexpected error: %v", i, err)
+			continue
+		}
+		got := [][2]string(f)
+		if diff := pretty.Compare(tt.want, got); diff != "" {
+			t.Errorf("case %d: diff=%s", i, diff)
+		}
+	}
+}
+
+func TestKVSliceSetError(t *testing.T) {
+	tests := []string{
+		"foo",
+		"foo,bar",
+	}
+
+	for i, tt := range tests {
+		var f KVSliceFlag
+		if err := f.Set(tt); err == nil {
+			t.Errorf("case %d: expected error, got nil", i)
+		}
+	}
+}
